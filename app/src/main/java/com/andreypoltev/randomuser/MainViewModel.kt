0a26fe0e027c1703c0854d66d272580100ef7784
andreypoltev <andreypoltev@gmail.com>
2024-01-26 15:51:19 +0300

    viewmodel

---
+++ app/src/main/java/com/andreypoltev/randomuser/MainViewModel.kt
@@ -0,0 +1,6 @@
+package com.andreypoltev.randomuser
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel:ViewModel() {
+}
---
+++ app/src/main/java/com/andreypoltev/randomuser/Navigatiion.kt
@@ -0,0 +1,8 @@
+package com.andreypoltev.randomuser
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Navigation(viewModel: MainViewModel) {
+
+}
